b6738a8f558ac0823a71d1d150beeb8cf89d9fa6
SOHAN-cyber <sohandogra703@gmail.com>
2022-06-16 00:19:12 +0530

    added changes

--- env/management/test.groovy
+++ env/management/test.groovy
@@ -1,11 +1,11 @@
 folder ('OKTS/INFRA')
 pipelineJob('OKTS/INFRA/INFRAJOB') {
     parameters {
-        choiceParam('Network', ['True', 'False'])
-        choiceParam('Openvpn', ['True', 'False'])
-        choiceParam('ALB', ['True', 'False'])
-        choiceParam('BP', ['True', 'False'])
-        choiceParam('EKS', ['True', 'False'])
+        choiceParam('Network', ['True', 'False'], 'Do you setup your own custom VPC' )
+        choiceParam('Openvpn', ['True', 'False'], 'Do you want setup your Openvpn Server')
+        choiceParam('ALB', ['True', 'False'], 'Do you want to setup  internal ALB')
+        choiceParam('BP', ['True', 'False'], 'Do you want to Setup BP Server')
+        choiceParam('EKS', ['True', 'False'], 'Do you want to setup EKS Cluster')
     }
     definition {
         cpsScm {
