ffc38a1847e1e6048fcf78e4aed6627ae0ee8d16
SOHAN-cyber <sohandogra703@gmail.com>
2022-06-15 23:56:58 +0530

    added changes

--- env/management/test.groovy
+++ env/management/test.groovy
@@ -14,7 +14,7 @@ pipelineJob('OKTS/INFRA/INFRAJOB') {
                         remote{
                             name('origin')
                             url('https://gitlab.com/ot-client/central-team/ot-orchid-international/infrastructure.git')
-                            credentials('a7090023-82ec-41cf-b990-166c0515a968')
+                            credentials('opstree')
                             branch('EKS')
                         }
                     }
