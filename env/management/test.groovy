b317d15bb72eb009c906403f446d1834ee64c697
SOHAN-cyber <sohandogra703@gmail.com>
2022-06-15 14:35:57 +0530

    changes for jenkins pipeline

---
+++ env/management/test.groovy
@@ -0,0 +1,39 @@
+folder('OKTS')
+folder('Infra')
+
+def NODEJSPIPELINEJOB = multibranchPipelineJob('OKTS/Infra')
+NODEJSPIPELINEJOB.with {
+    description 'Job to setup Testing'
+        branchSources {
+            branchSource {
+                source {
+                    git{
+                        id('02')
+                        remote('https://gitlab.com/ot-client/central-team/ot-orchid-international/infrastructure.git')
+		                credentialsId('opstree')
+                    }
+                }
+                strategy {
+                    defaultBranchPropertyStrategy {
+                        props {
+                            noTriggerBranchProperty()
+                        }
+                    }
+                }
+            }
+        }
+        configure {
+            def traits = it / 'sources' / 'data' / 'jenkins.branch.BranchSource' / 'source' / 'traits'
+            traits << 'jenkins.plugins.git.traits.BranchDiscoveryTrait' {}
+        }
+        factory {
+            workflowBranchProjectFactory {
+                scriptPath('env/management/network/Jenkinsfile')
+            }
+        }
+        orphanedItemStrategy {
+            discardOldItems {
+                numToKeep(20)
+            }
+        }
+    }
