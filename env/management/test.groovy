4c87c55573057e0651ab7eef0002ec9a1fb1f77e
SOHAN-cyber <sohandogra703@gmail.com>
2022-06-15 22:31:39 +0530

    changes in groovy script

--- env/management/test.groovy
+++ env/management/test.groovy
@@ -1,39 +1,21 @@
-folder('OKTS')
-folder('OKTS/Infra')
-
-def INFRAJOB = multibranchPipelineJob('OKTS/Infra/INFRAJOB')
-INFRAJOB.with {
-    description 'Job to setup Testing'
-        branchSources {
-            branchSource {
-                source {
-                    git{
-                        id('02')
-                        remote('https://gitlab.com/ot-client/central-team/ot-orchid-international/infrastructure.git')
-		                credentialsId('a7090023-82ec-41cf-b990-166c0515a968')
-                    }
-                }
-                strategy {
-                    defaultBranchPropertyStrategy {
-                        props {
-                            noTriggerBranchProperty()
+folder ('INFRA')
+pipelineJob('INFRA/SingleNetworkSetupJob') {
+    parameters {
+        choiceParam('Network', ['True', 'False'])
+    }
+    definition {
+        cpsScm {
+                scm {
+                    git {
+                        remote{
+                            name('origin')
+                            url('https://gitlab.com/ot-client/central-team/ot-orchid-international/infrastructure.git')
+                            credentials('a7090023-82ec-41cf-b990-166c0515a968')
+                            branch('EKS')
                         }
                     }
                 }
-            }
-        }
-        configure {
-            def traits = it / 'sources' / 'data' / 'jenkins.branch.BranchSource' / 'source' / 'traits'
-            traits << 'jenkins.plugins.git.traits.BranchDiscoveryTrait' {}
-        }
-        factory {
-            workflowBranchProjectFactory {
-                scriptPath('env/management/network/Jenkinsfile')
-            }
-        }
-        orphanedItemStrategy {
-            discardOldItems {
-                numToKeep(20)
-            }
+            scriptPath('env/management/network/Jenkinsfile')
         }
     }
+}
